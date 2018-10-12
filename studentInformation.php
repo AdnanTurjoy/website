90adda3ea2291d4b68b137fe0ff9d389daf0f2d2
Adnan Turjoy <aturjoy4@gmail.com>
2018-10-12 23:13:50 +0600

    Add files via upload

---
+++ studentInformation.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css"  crossorigin="anonymous">
+
+    <title>Student Information</title>
+  </head>
+  <body>
+          <center><h5 class="nws1 display-4">OUR ALL STUDENT INFORMATION</h5></center>
+    <p>
+  <center><button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+    Button with data-target   <!-- ekhane indivitual student er pic thakbe and click korle niche show kore -->
+  </button></center>
+</p>
+<div class="collapse" id="collapseExample">
+  <div class="card card-body">
+    Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe Student er data show korbe
+  </div>
+</div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   <script src="js/jq1.js" crossorigin="anonymous"></script>
+    <script src="js/jq2.js" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+  </body>
+</html>
